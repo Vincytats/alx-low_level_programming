b01ed34f6df25f9bb1164d426b2098405a76d3d2
Vincytats <vincenttats@gmail.com>
2023-09-07 18:04:21 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - thiis is a printf function
+ * Return:0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a price of art,\n");
+return (0);
+}
