19c683f88ebfa439719790caab25807a0ec132d2
Vincytats <vincenttats@gmail.com>
2023-09-08 18:43:32 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function of the task
+ * Return: always zero
+ */
+int main(void)
+{
+int number = 0;
+while (number <= 9)
+{
+printf("%d\n", number);
+number++;
+}
+return (0);
+}
