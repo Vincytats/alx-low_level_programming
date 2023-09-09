db1c46913e5d846da78e2c262454c12224402bcc
Vincytats <vincenttats@gmail.com>
2023-09-08 18:08:53 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main this is the function of the task
+ *Return: 0
+ */
+int main(void)
+{
+char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar('\n');
+return (0);
+}
