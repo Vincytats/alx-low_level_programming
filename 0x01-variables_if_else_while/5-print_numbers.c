3dc6c8866e0a5a4ae10a039593b2f0ccaf61df67
Vincytats <vincenttats@gmail.com>
2023-09-08 20:15:55 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,12 +5,15 @@
  */
 int main(void)
 {
-char letter;
-for (letter = 'a'; letter <= 'z'; letter++)
-{
-if (letter != 'e' && letter != 'q')
-putchar(letter);
-}
-putchar('\n');
+printf("0\n");
+printf("1\n");
+printf("2\n");
+printf("3\n");
+printf("4\n");
+printf("5\n");
+printf("6\n");
+printf("7\n");
+printf("8\n");
+printf("9\n");
 return (0);
 }
