5bfd19894793a0764b83be44a1e00f428f692de6
Vincytats <vincenttats@gmail.com>
2023-09-08 20:22:54 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,15 +5,10 @@
  */
 int main(void)
 {
-printf("0\n");
-printf("1\n");
-printf("2\n");
-printf("3\n");
-printf("4\n");
-printf("5\n");
-printf("6\n");
-printf("7\n");
-printf("8\n");
-printf("9\n");
+int number;
+for (number = 0; number <= 9; number++)
+{
+printf("%d\n", number);
+}
 return (0);
 }
