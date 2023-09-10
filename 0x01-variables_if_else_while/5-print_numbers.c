6729fa09c846a71daff37a64352d2777374de3e5
Vincytats <vincenttats@gmail.com>
2023-09-10 14:33:19 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - function of the task
  * Return: always zero
  */
 int main(void)
 {
-for (int i = 0; i <= 9; i++)
-{
-printf("%d\n", i);
-}
+int i;
+for (i = 0; i < 10; i++)
+printf("%d", i);
+printf("\n");
+
 return (0);
 }
