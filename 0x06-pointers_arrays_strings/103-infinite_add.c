32c696f3b36d55fe6d23119a4600614b746d2ef6
Vincytats <vincenttats@gmail.com>
2023-09-21 16:58:29 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -34,7 +34,6 @@ temp = *(n + j);
  * @size_r: buffer size
  * Return: pointer to calling function
  */
-
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 int overflow = 0, i = 0, j = 0, digits = 0;
@@ -53,11 +52,11 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - '0';
+val1 = *(n1 + i) -'0';
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - '0';
+val2 = *(n2 + j) -'0';
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
