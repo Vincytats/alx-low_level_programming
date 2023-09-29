5b5e0cb87a23ddc2579bb869d4e83c82e517efe0
Vincytats <vincenttats@gmail.com>
2023-09-29 16:57:39 -0700

    Write a program that

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,14 +1,14 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
+
 /**
- * main: prints its name
- * @argc: argument count
- * @argv: argument vector
- * Return: 0
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-(void) argc;
-printf("%s\n", argv[0]);
+printf("%s\n", *argv);
 return (0);
 }
