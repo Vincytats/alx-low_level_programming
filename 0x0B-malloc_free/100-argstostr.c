47f9474c269bb51cc637804e04d74115a7c57aa3
Vincytats <vincenttats@gmail.com>
2023-10-03 16:32:56 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,7 +12,7 @@ char *argstostr(int ac, char **av)
 {
 int ch = 0, i = 0, j = 0, k = 0;
 char *s;
-if (ac ==0 || av == NULL)
+if (ac == 0 || av == NULL)
 return (NULL);
 while (i < ac)
 {
@@ -24,16 +24,17 @@ j++;
 j = 0;
 i++;
 }
-s = malloc((sizeof(char) * ch) + ac +1);
+s = malloc((sizeof(char) * ch) +ac + 1);
 i = 0;
 while (av[i])
 {
 while (av[i][j])
-{s[k] = av[i][j];
+{
+s[k] = av[i][j];
 k++;
 j++;
 }
-s[k] = '\0';
+s[k] = '\n';
 j = 0;
 k++;
 i++;
