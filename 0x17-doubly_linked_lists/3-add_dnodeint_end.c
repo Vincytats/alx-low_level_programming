7ce83f0c101d1349a85bbe9fd2ab67a454f221ec
Vincytats <vincenttats@gmail.com>
2023-12-07 11:15:13 -0800

    C

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * add_dnodeint_end - adds a new node at the end
+ *of a dlistint_t list
  * @head: head of the list
  * @n: value of the element
  * Return: the address of the new element
